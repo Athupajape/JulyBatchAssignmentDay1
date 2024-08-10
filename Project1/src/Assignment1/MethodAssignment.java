25dc73b8dbca5e4a2f0d8bddf257b766d7d94ba7
atharva <atharvaparanjape2@gmail.com>
2024-08-10 10:10:04 +0530

    Method Assignment commit

---
+++ Project1/src/Assignment1/MethodAssignment.java
@@ -0,0 +1,41 @@
+package Assignment1;
+
+public class MethodAssignment {
+
+	//Default Method
+	public void Defaultmethod1() {
+		this.Method3(88, 99, 110);
+		System.out.println("default method");
+	}
+	
+	//One Parameter Method
+	public void Method1(int a) {
+		this.Defaultmethod1();
+		System.out.println("one parameterized method");
+	}
+	
+	//Two Parameter Method
+	public void Method2(int b,int c) {
+		this.Method1(77);
+		System.out.println("two parameterized method");
+	}
+	
+	//Three Parameter Method
+	public void Method3(int p,int q,int r) {	
+		System.out.println("three parameterized method");
+	}
+
+	//Four Parameter Method
+	public void Method4(int m,int n,int z, int y) {
+		this.Method2(55,66);
+		System.out.println("four parameterized method");
+		
+	}
+	
+	public static void main(String[] args) {
+		MethodAssignment ob=new MethodAssignment();
+		ob.Method4(11,22,33,44);
+	}
+	
+	
+}
